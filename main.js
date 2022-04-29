97586fd4f080a9ddc4c775361eebac841c273498
Tom <tom.miseur@gmail.com>
2022-04-29 15:59:00 -0600

    Minor comment addition

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import { submitCheckout } from "./submitCheckout.js";
 
 export const options = {};
 
+// used to store global variables
 const vars = [];
 
 // global min/max sleep durations (in seconds):
